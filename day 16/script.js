41a9c625e464c88946837069449b7da30c1f9e58
Koseen <543662977@qq.com>
2019-05-24 10:11:51 +0800

    第十六天，开始感受JS的乐趣

---
+++ day 16/script.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById('name').innerHTML);
+
+document.getElementById('more').onclick = function() {
+	// body...
+	document.getElementById('name')innerHTML += '知乎ID';
+}
+
